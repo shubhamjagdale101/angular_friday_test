c5e6dcb3c897f4909a0f2ad1ad5c99ead815c757
shubhamjagdale101 <shubhamjagdale101@gmail.com>
2024-09-13 13:01:41 +0530

    assignment completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'shubham_jagdale';
+  comp = ""
+  data = "Welcome to the page Home"
+  str = "Welcome to the page Home"
+  userName = ""
+  show_selected = ""
+
+  handleClick(str : any){
+    this.comp = 'Home'
+    if(this.userName !== "") this.str = `Welcome to the page ${str}`
+    else this.str = `Welcome to the page ${str}`
+    this.data = `Welcome to the page ${str}`
+  }
+
+  handleChange(){
+    if(this.userName !== "") this.str = `Mr/Mrs ${this.userName} ${this.data}`
+  }
 }
